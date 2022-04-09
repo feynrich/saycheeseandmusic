3a018df18d1937c80e54629aa8faa47d4e5503e5
mille <milleryan2003@gmail.com>
2022-04-09 15:28:31 +0300

    good

--- saycheeseandmusic/main/models.py
+++ saycheeseandmusic/main/models.py
@@ -1,3 +1,18 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    song_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=2000)
+    singer = models.CharField(max_length=2000)
+    album = models.CharField(max_length=2000)
+    song_img = models.ImageField(upload_to='images')
+    song_file = models.FileField(upload_to='images')
+    duration = models.CharField(max_length=20)
+    paginate_by = 2
+
+    def __str__(self):
+        return self.name
+
+
--- saycheeseandmusic/main/views.py
+++ saycheeseandmusic/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from .models import Song
 
 def home(request):
     return render(request, 'main/home.html')
@@ -10,4 +11,8 @@ def audio_library(request):
 
 
 def developers_info(request):
-    return render(request, 'main/developers.html')
+    paginator= Paginator(Song.objects.all(),1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context={"page_obj": page_obj}
+    return render(request,"main/test.html", context)
